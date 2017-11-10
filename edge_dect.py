141c1ea423a7f351de4fa392ba008eecbfd5abbf
mattclifford1 <mattc863@hotmail.com>
2017-11-10 14:09:02 +0000

    trying canny edge dectoer

--- edge_dect.py
+++ edge_dect.py
@@ -5,35 +5,33 @@ import matplotlib.image as mpimg
 from scipy.misc import imread
 
 
-
 image = cv2.imread("test.jpg")
 imageO = image
 image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# r = image.copy()
-# # set blue and green channels to 0
-# r[:, :, 0] = 0
-# r[:, :, 1] = 0
 
-# cv2.imshow('R-RGB', r)
-# cv2.waitKey(0)
+imageRed = image[:, :, 0] #only take red
+imageRed_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-image[:, :, 2] = 0  #green to zero
-image[:, :, 1] = 0  #blue to zero
-
-image = image[:, :, 0]
-
-laplacian = cv2.Laplacian(image,cv2.CV_64F)
-sobelx = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
-sobelxy = cv2.Sobel(image,cv2.CV_64F,1,1,ksize=5)
+edges = cv2.Canny(imageRed_grey,200,100)
+laplacian = cv2.Laplacian(imageRed,cv2.CV_64F)
+sobelx = cv2.Sobel(imageRed,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(imageRed,cv2.CV_64F,0,1,ksize=5)
+sobelxy = cv2.Sobel(imageRed,cv2.CV_64F,1,1,ksize=5)
 
 plt.subplot(2,2,1),plt.imshow(image_grey,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(image,cmap = 'gray')
-plt.title('Only Red Channel'), plt.xticks([]), plt.yticks([])
+plt.title('Original')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
+plt.subplot(2,2,2),plt.imshow(imageRed_grey,cmap = 'gray')
+plt.title('Only Red Channel')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
 plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X Direction'), plt.xticks([]), plt.yticks([])
+plt.title('Sobel X Direction')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+
 plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-plt.title('Sobel Y Direction'), plt.xticks([]), plt.yticks([])
+plt.title('Sobel Y Direction')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 
 plt.show()
