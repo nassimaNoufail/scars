347091b9056d194705e6fa7e9dff5a16a3eb953f
unknown <tw15220@my.bristol.ac.uk>
2017-11-23 10:33:30 +0000

    extracts only red part

--- colourStan.m
+++ colourStan.m
@@ -11,41 +11,58 @@ mim(:,300-d:300,:)=colour;
 mim(1:f,:,:)=colour;
 mim(336-f:336,:,:)=colour;
 
-
+figure
 imshow(im)
 
 n=nnz(mim(:,:,1));
 
 [row,col]=find(mim(:,:,1),1);
 
-
+myRow=163;
+myCol=146;
+for i=1:175-163
+    RGB([mim(myRow,myCol,1),mim(myRow,myCol,2),mim(myRow,myCol,3)]);
+    myRow=myRow+1;
+end
+%%{
 for i=1:336
     for j=1:300
-        differM(i,j)=RGB([im(i,j,1),im(i,j,2),im(i,j,3)]);
+        differM(i,j)=double(RGB([mim(i,j,1),mim(i,j,2),mim(i,j,3)]));
     end
 end
+%}
 [aa,bb]=find(differM);
-for i=1:336
-    for j=1:300
-        if differM(i,j)>240
-        	lol=0;
-        else
-            im(i,j,:)=0;
-        end
-    end
-end
+[aa,bb];
 
 
-%sum(sum(mim(:,:,1)))/n;
-%sum(sum(mim(:,:,2)))/n;
-%sum(sum(mim(:,:,3)))/n;
-
-figure
-imshow(im)
+redExtract(im)
 
 
 function result=RGB(vec)
 vec=num2cell(vec);
 [R,G,B]=vec{:};%unpack
+R=double(R);
+G=double(G);
+B=double(B);
 result=(R-G)*(R-B);
+end
+
+function redExtract(image)
+for i=1:336
+    for j=1:300
+        differM(i,j)=RGB([image(i,j,1),image(i,j,2),image(i,j,3)]);
+    end
+end
+%[aa,bb]=find(differM);
+for i=1:336
+    for j=1:300
+        if differM(i,j)>10000
+        	lol=0;
+        else
+            image(i,j,:)=0;
+        end
+    end
+end
+figure
+imshow(image)
 end
