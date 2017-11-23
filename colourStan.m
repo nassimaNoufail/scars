880c6e6fb2550d9b0ab4e5a69af4e4e6e2e852dc
Tristan Winstanley <tw15220@bristol.ac.uk>
2017-11-23 15:42:58 +0000

    bla

--- colourStan.m
+++ colourStan.m
@@ -1,6 +1,15 @@
-close all
-im= imread('cokeCan2.jpg');
-[TC,TR,lol]=size(im);
+%close all
+figure
+im= imread('cokeCan.jpg');
+
+subplot(2,2,1)%%%%%%%%%%%
+imshow(im)
+title('OG')
+
+
+[TR,TC,lol]=size(im);
+
+trueColour=244; %coke red
 
 mim=im;
 [imR,imC]=size(mim(:,:,1));
@@ -8,13 +17,12 @@ d=145;
 f=160;
 colour=0;
 mim(:,1:d,:)=colour;
-mim(:,TR-d:TR,:)=colour;
+mim(:,TC-d:TC,:)=colour;
 
 mim(1:f,:,:)=colour;
-mim(TC-f:TC,:,:)=colour;
+mim(TR-f:TR,:,:)=colour;
 
-%figure
-%imshow(im)
+
 
 n=nnz(mim(:,:,1));
 
@@ -33,21 +41,39 @@ for i=1:TC
     end
 end
 %}
-[aa,bb]=find(differM);
+%[aa,bb]=find(differM);
 
 
 
-gim=redExtract(im,TC,TR);
-gim(:,:,1);
-total=nnz(gim(:,:,1));
-mean1=sum(sum(gim(:,:,1)))/total
-mean2=sum(sum(gim(:,:,2)))/total
-mean3=sum(sum(gim(:,:,3)))/total
-figure
+gim=redExtract(im,TR,TC);
+
+subplot(2,2,2)%%%%%%%%%%
 imshow(gim)
-gim(:,:,1)=gim(:,:,1)+40;
-figure
+
+title('Red Extractor')
+totalNonZ=nnz(gim(:,:,1));%total non zero entries
+meanR=sum(sum(gim(:,:,1)))/totalNonZ;
+diff=trueColour-meanR;
+
+%mean2=sum(sum(gim(:,:,2)))/total;
+%mean3=sum(sum(gim(:,:,3)))/total;
+
+
+for i=1:TR
+    for j=1:TC
+        if gim(i,j,1)>10
+            gim(i,j,1)=gim(i,j,1)+diff;
+        end
+    end
+end
+
+subplot(2,2,4)
 imshow(gim)
+title('Final')
+
+
+
+
 function result=RGB(vec)
 vec=num2cell(vec);
 [R,G,B]=vec{:};%unpack
@@ -57,16 +83,17 @@ B=double(B);
 result=(R-G)*(R-B);
 end
 
-function image=redExtract(image,TC,TR)
-for i=1:TC
-    for j=1:TR
+function image=redExtract(image,TR,TC)
+
+for i=1:TR
+    for j=1:TC
         differM(i,j)=RGB([image(i,j,1),image(i,j,2),image(i,j,3)]);
     end
 end
-%[aa,bb]=find(differM);
-for i=1:TC
-    for j=1:TR
-        if differM(i,j)>10000
+
+for i=1:TR
+    for j=1:TC
+        if differM(i,j)>12000
         	lol=0;
         else
             image(i,j,:)=0;
