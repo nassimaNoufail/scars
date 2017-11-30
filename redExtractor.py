7fe21a033038ccb2eec950dc8a16be7ce9c16ea4
unknown <tw15220@my.bristol.ac.uk>
2017-11-30 00:49:19 +0000

    increases red

--- redExtractor.py
+++ redExtractor.py
@@ -6,23 +6,7 @@ from scipy.misc import imread
 
 
 def main():
-    '''
-    mean1=sum(sum(redImg(:,:,1)))/total;
-    mean2=sum(sum(redImg(:,:,2)))/total;
-    mean3=sum(sum(redImg(:,:,3)))/total;
-    
-    figure
-    imshow(gim)
-    
-    gim(:,:,1)=gim(:,:,1)+(trueColour-mean1);
-    figure
-    imshow(gim)
-    '''
-    
-    
-    
-    
-    
+
     imageArray=['cokeCan.jpg',
                 'Test_data/47/47_3.png','54_UF_TW_SMALL.jpg']
     imageNb=len(imageArray)
@@ -47,6 +31,7 @@ def main():
         myPlot2.axis('off') 
         plt.imshow(redImg)
     '''
+    plt.figure(0)
     img=plt.imread(imageArray[2])
     TR,TC,unused=np.shape(img)
     redImg=redExtract(img,TR,TC)
@@ -57,10 +42,24 @@ def main():
     plt.subplot(1,2,2), plt.imshow(redImg)
     plt.axis('off')
     
+    total=np.count_nonzero(redImg[:,:,0])
     
+    mean0=np.sum(redImg[:,:,0])/total  #  mean of red pixels
     
+    for i in range(0,TR):
+        for j in range(0,TC):
+            if redImg[i,j,0]!=0:
+                if redImg[i,j,0]+(trueColour-mean0)>255:
+                    redImg[i,j,0]=255
+                else:
+                    redImg[i,j,0]+=(trueColour-mean0)     
+                      
+    plt.figure(1)
+    plt.imshow(redImg)
+    meanFinal=np.sum(redImg[:,:,0])/total
+    print('meanfinal=',meanFinal)
     
-    #total=np.count_nonzero(redImg[:,:,0])
+
 def redExtract(image,TR,TC):
     differM=np.zeros([TR,TC])
     for i in range(0,TR):
@@ -69,7 +68,7 @@ def redExtract(image,TR,TC):
             
     for i in range(0,TR):
         for j in range(0,TC):
-            if differM[i,j]<10000:
+            if differM[i,j]<9000:
                 image[i,j,:]=0
     return image    
               
